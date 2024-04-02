4fd0b93770f242bfe06a1cb451b22b9a7a829094
J.T. Mints <cyfoxstudios@gmail.com>
2024-04-02 15:52:08 -0700

    Initialize Project Initializes project using Xcode's 'Tabbed Application' template to create an application with two views and a menu at the bottom that will switch between them by clicking on a button corresponding to each view.

---
+++ usher-helper/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  usher-helper
+//
+//  Created by Zephyr on 4/2/24.
+//  Copyright © 2024 Cyberfox Studios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
